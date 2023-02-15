81e901083337407f6aa67c58967227bb53827127
paulphysio <obinnap350@gmail.com>
2023-02-15 11:46:49 +0100

    testinga

--- sender/views.py
+++ sender/views.py
@@ -59,31 +59,55 @@ def sendMail(request, pk):
     receiver_list=[]
     for i in range(2, wa.max_row+1):
         list=[]
-        if counter <= 20:
+        for j in range(1, wa.max_column+1):
+            cell_obj = wa.cell(row=i, column=j)
+            x = wa.cell(row=i, column=j).value
+            list.append(x)
+            if j==(wa.max_column):
+                reallist.append(list)
+                
+        receiver = str(wa.cell(row=i, column=email_index+ 1).value)
+        receiver_list.append(receiver)
 
-            for j in range(1, wa.max_column+1):
-                cell_obj = wa.cell(row=i, column=j)
-                x = wa.cell(row=i, column=j).value
-                list.append(x)
-                if j==(wa.max_column):
-                    reallist.append(list)
-                    
-            receiver = str(wa.cell(row=i, column=email_index+ 1).value)
-            receiver_list.append(receiver)
-
-        else:
-            # Add a delay of 1 minute if the counter is over 20
-            time.sleep(5)
-            counter = 0
+       
+       
+    def send_emails_with_threads():
+    # Set up threads
+        num_threads = 10
+        threads = []
+        chunk_size = len(receiver_list) // num_threads
+        for i in range(num_threads):
+            start = i * chunk_size
+            end = start + chunk_size
+            if i == num_threads - 1:
+                end = len(receiver_list)
+            chunk_receivers = receiver_list[start:end]
+            chunk_messages = reallist[start:end]
+            t = threading.Thread(target=send_chunk_emails, args=(chunk_receivers, chunk_messages))
+            threads.append(t)
         
-    with concurrent.futures.ThreadPoolExecutor(max_workers=13) as executor:
-        for i in range(len(receiver_list)):
-            recipient = receiver_list[i]
-            body = reallist[i]
-            executor.submit(send_email, recipient, body)
-    
+        # Start threads
+        start_time = time.time()
+        for t in threads:
+            t.start()
+        
+        # Wait for threads to finish
+        for t in threads:
+            t.join()
+        
+        # Calculate time taken to send emails
+        time_taken = time.time() - start_time
+        print(f"Sent {len(receiver_list)} emails in {time_taken} seconds.")
+        
+    # Define function for sending a chunk of emails
+    def send_chunk_emails(receivers, messages):
+        for i in range(len(receivers)):
+            send_email(receivers[i], messages[i])
+            if i % 10 == 0:
+                time.sleep(1) # Sleep for 1 second every 10 emails
 
-    print('All emails sent.')
+    # Call function to send emails with threads
+    send_emails_with_threads()
     
     if request.method == 'POST':
 
